4ae789f942f1f6d30707b5138016f508e3ca9dde
Tupapi <2122200380@soy.utj.edu.mx>
2024-09-18 01:20:52 -0600

    algunas cositas

--- app/src/main/java/com/example/prctica02/TripDetailsActivity.kt
+++ app/src/main/java/com/example/prctica02/TripDetailsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.prctica02
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class TripDetailsActivity : AppCompatActivity() {
 
@@ -12,8 +14,22 @@ class TripDetailsActivity : AppCompatActivity() {
 
         val travelDetails = intent.getParcelableExtra<TravelDetails>("travelDetails")
 
+        val imageViewDestination = findViewById<ImageView>(R.id.imageViewDestination)
         val textViewDetails = findViewById<TextView>(R.id.textViewDetails)
+
         travelDetails?.let {
+            // Establecer la imagen del destino
+            val imageResId = when (it.destination) {
+                "Playas de México" -> R.drawable.playas_mexico
+                "Montañas de Canadá" -> R.drawable.montanas_canada
+                "París" -> R.drawable.paris
+                "Roma" -> R.drawable.roma
+                "Japón" -> R.drawable.japon
+                else -> R.drawable.logo
+            }
+            imageViewDestination.setImageDrawable(ContextCompat.getDrawable(this, imageResId as Int))
+
+            // Establecer los detalles del viaje
             val details = """
                 Destino: ${it.destination}
                 Hotel: ${it.hotelName}
